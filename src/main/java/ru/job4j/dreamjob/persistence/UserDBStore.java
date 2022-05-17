a7d65c01db2a920cb0d4af0c9c838fd66540cd10
staskorobeynikov <stas.korobeinikov@mail.ru>
2022-05-17 19:11:00 +0300

    fix bug with authfilter

--- src/main/java/ru/job4j/dreamjob/persistence/UserDBStore.java
+++ src/main/java/ru/job4j/dreamjob/persistence/UserDBStore.java
@@ -9,6 +9,7 @@ import ru.job4j.dreamjob.model.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 @Repository
